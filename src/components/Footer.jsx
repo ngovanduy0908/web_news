f8512576fa3595ef2fbdb78e003de5d174815984
ngovanduy0908 <ngoduy090801@gmail.com>
2023-04-18 13:42:44 +0700

    dev-feat-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,213 @@
 import React from "react";
-import { FaBeer } from "react-icons/fa";
+import { TbBrandStackshare } from "react-icons/tb";
+import { BsLaptop } from "react-icons/bs";
+import { AiOutlineMobile } from "react-icons/ai";
+import { BiUpArrow } from "react-icons/bi";
 const Footer = () => {
   return (
-    <footer class="bg-white rounded-lg shadow  dark:bg-gray-800">
-      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" class="hover:underline">
-            <FaBeer />
-          </a>
-          . All Rights Reserved.
-        </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-          <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">
-              About
+    <>
+      <footer className=" dark:bg-gray-900 footer pb-4">
+        <div className="mx-auto  max-w-screen-xl w-[1080px]">
+          <div className="grid grid-cols-2 gap-8 px-4 pb-6 pt-4 lg:pb-8 lg:pt-4 md:grid-cols-4">
+            <div>
+              <h2 class="mb-10 font-semibold text-gray-700 dark:text-white uppercase text-xl">
+                Sở / Ban Ngành
+              </h2>
+              <ul class="text-gray-700 dark:text-gray-400 font-medium">
+                <li class="mb-1">
+                  <a href="#" class=" hover:underline">
+                    Sở Giáo Dục và Đào Tạo
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Sở khoa học và công nghệ
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Sở NN&PT Nông thôn
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Sở công thương
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Sở tài chính
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 class="mb-10  font-semibold text-gray-700 dark:text-white uppercase text-xl">
+                Bộ / Ngành
+              </h2>
+              <ul class="text-gray-700 dark:text-gray-400 font-medium">
+                <li class="mb-1">
+                  <a href="#" class=" hover:underline">
+                    Bộ Nông Nghiệp
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Bộ Giáo Dục
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Bộ Tài Chính
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Bộ Giao thông vận tải
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Bộ Y tế
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 class="mb-10  font-semibold text-gray-700 dark:text-white uppercase text-xl">
+                ĐOÀN HỘI
+              </h2>
+              <ul class="text-gray-700 dark:text-gray-400 font-medium">
+                <li class="mb-1">
+                  <a href="#" class=" hover:underline">
+                    Liên hiệp các hội KHKT
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    LH Các tổ chức hữu nghị
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Ngân hàng chính sách xã hội
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Đoàn thanh niên Việt Nam
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Hội tin học Việt Nam
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h2 class="mb-10  font-semibold text-gray-700 dark:text-white uppercase text-xl">
+                LIÊN KẾT WEBSITE
+              </h2>
+              <ul class="text-gray-700 dark:text-gray-400 font-medium">
+                <li class="mb-1">
+                  <a href="#" class=" hover:underline">
+                    Đảng Cộng sản Việt Nam
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Cổng thông tin Chính Phủ
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Báo điện tử Chính phủ
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Thông tấn xã Việt Nam
+                  </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class="hover:underline">
+                    Đài tiếng nói Việt Nam
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="px-4 pt-6  text-gray-700">
+            <div className="text-center">
+              <span className="uppercase text-xl">
+                <strong>
+                  Ban biên tập Cổng thông tin điện tử Hội Doanh Nhân Thanh Hóa
+                  Tại Hà Nội
+                </strong>
+              </span>
+              <p>Điện thoại: 0982 885588</p>
+              <p>Fax: 0982 885588</p>
+              <p>
+                Email: <a href="#">hoidoanhnhanthanhhoa.hbta@gmail.com</a>
+              </p>
+            </div>
+
+            <div className="text-center">
+              <div className="text-center">
+                © 2022&nbsp;- Bản quyền&nbsp;thuộc về Hội Doanh Nhân Thanh Hóa
+                tại Hà Nội.
+                <br />
+                <a href="">Điều khoản sử dụng</a>" | "
+                <a href="">Chính sách bảo mật (Quyền riêng tư)</a>
+              </div>
+            </div>
+
+            <div className="text-center">
+              <p>
+                Powered by
+                <a href="">NukeViet eGovernment</a>. Designed by
+                <a href="">VINADES.,JSC</a>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="bg-gray-500 py-4">
+        <div className="w-[1080px] m-auto flex  justify-between items-center">
+          <div className="flex items-center">
+            <a
+              href="#"
+              className="mr-3 w-7 h-7 rounded-full flex items-center bg-sky-500 text-white justify-center"
+            >
+              <TbBrandStackshare className="font-bold text-lg" />
             </a>
-          </li>
-          <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
-              Privacy Policy
+            <a
+              href="#"
+              className="mr-3 w-7 h-7 rounded-full flex items-center bg-sky-500 text-white justify-center"
+            >
+              <BsLaptop className="font-bold text-lg" />
             </a>
-          </li>
-          <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
-              Licensing
+            <a
+              href="#"
+              className="mr-3 w-7 h-7 rounded-full flex items-center bg-sky-500 text-white justify-center"
+            >
+              <AiOutlineMobile className="font-bold text-lg" />
             </a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">
-              Contact
+          </div>
+          <div>
+            <a
+              href="#"
+              className="w-8 h-8 rounded-full flex items-center bg-sky-500 text-white justify-center"
+            >
+              <BiUpArrow className="font-bold text-lg" />
             </a>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-    </footer>
+    </>
   );
 };
 
