7cfba95c4e6f3788e3344c2359019bf3c1765143
Dunga1a <dunglm363@gmail.com>
2023-04-18 16:57:04 +0700

    feat-navbar

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useState } from "react";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <Header />
+      <div className="h-[44px] bg-[#007fe4] absolute top-[33px] w-[calc((100%-1080px)/2)]"></div>
+      <div className="h-[44px] bg-[#007fe4] absolute top-[33px] w-[calc((100%-1080px)/2)] right-0"></div>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
